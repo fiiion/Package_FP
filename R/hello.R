aa9292b0d5e42603912acd60a1f930f2e004203f
fiiion <76860271+fiiion@users.noreply.github.com>
2022-05-14 13:17:54 -0500

    one function down... one to go

--- R/hello.R
+++ R/hello.R
@@ -24,8 +24,21 @@ hello <- function() {
 # (preferably) city boundaries. Then it will crop the data to the boundaries.
 
 
-# step 1: setting the CRS -------------------------------------
-city_census_data <- funtion(census, bounds) {  ## census == census data, bounds == boundaries (shp., etc)
+
+city_census_data <- function(census, bounds) {  ## census == census data, bounds == boundaries (shp., etc)
+  ### step 1: setting the CRS -------------------------------------
+  # we need the CRS of the census data to match boundary data
+  # we'll choose to use the census CRS, as this will be consistent
+
+  # creating a new object -- boundary with amended CRS
+  bounds_CRS <- st_transform(bounds, st_crs(census))
+
+  ### step 2: intersecting the bounds and the data
+  city_int <- st_intersection(census, bounds_CRS)
+
+  ## this ^ will be returned automatically! yay
+  # now I'll try to add documentation...
+
 
 
 
