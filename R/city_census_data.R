ef737fd05a88517adc7447a19cce6e03c4dd2e00
fiiion <76860271+fiiion@users.noreply.github.com>
2022-05-14 14:05:19 -0500

    completed scripts with documentation !

--- R/city_census_data.R
+++ R/city_census_data.R
@@ -1,3 +1,22 @@
+#' Intersect census data with boundaries
+#
+#'
+#' @param census_var table with the census variable of interest and associated geometries
+#' can use clean_census_data to get this input
+#'
+#' (created for census tracts & city boundaries, should work for other sizes)
+#'
+#'
+#' @param bounds the boundaries to intersect with the census variable
+#'
+#' @return city_int -- the intersected geometries with the boundary
+#' @export
+#'
+#' @examples
+#'
+#'
+#'
+#'
 city_census_data <- function(census_var, bounds) {
   ## census == census data, bounds == boundaries (shp., etc)
 
@@ -6,10 +25,10 @@ city_census_data <- function(census_var, bounds) {
   # we'll choose to use the census CRS, as this will be consistent
 
   # creating a new object -- boundary with amended CRS
-  bounds_CRS <- st_transform(bounds, st_crs(census))
+  bounds_CRS <- st_transform(bounds, st_crs(census_var))
 
   ### step 2: intersecting the bounds and the data ----------------
-  city_int <- st_intersection(census, bounds_CRS)
+  city_int <- st_intersection(census_var, bounds_CRS)
 
   ## this ^ will be returned automatically! yay
   # now I'll try to add documentation...
--- R/clean_census_data.R
+++ R/clean_census_data.R
@@ -2,6 +2,25 @@
 
 ## Function 1: cleaning census data:
 
+#' Cleaning Census data -- removing all but relevant variable and geometries
+#'
+#' @param census_table the table of census values extracted with API key
+#'
+#'  example -- pulling in census data: it should look something like this:
+#'
+#'  mpls_income2019 <- get_acs(geography = "tract", year = 2019,
+#'  variables = c(med_earn = "S2001_C01_002E"), state = "MN",
+#'  county = "Hennepin", survey = "acs5", output = "wide",
+#'  geometry = TRUE)
+#'
+#'
+#' @param variable_n the variable of interest, should be a character string
+#'
+#' @return a clean table with only the census variable and its geometries
+#' Note: can be used int eh city_census_data function
+#' @export
+#'
+#' @examples
 clean_census_data <- function(census_table, variable_n) {
   ## census is the census data, variable_n is the variable of interest
   # variable_n should be input as a character string!
