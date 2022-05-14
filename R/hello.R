1659af25a7ea6ef371ca98beb3e4befe19c34643
fiiion <76860271+fiiion@users.noreply.github.com>
2022-05-14 13:33:21 -0500

    added second function (still need proper documentation)

--- R/hello.R
+++ R/hello.R
@@ -18,14 +18,41 @@ hello <- function() {
 }
 
 
-#### attempting to generalize an algorithm used in my final project
+#### attempting to generalize some functions used in my final project
+
+## Function 1: cleaning census data:
+
+clean_census_data <- function(census_table, variable_n) {
+  ## census is the census data, variable_n is the variable of interest
+  # variable_n should be input as a character string!
+
+  ##### example -- pulling in census data: it should look something like this
+    #mpls_income2019 <- get_acs(geography = "tract",
+                             #year = 2019,
+                             #variables = c(med_earn = "S2001_C01_002E"),
+                             #state = "MN",
+                             #county = "Hennepin",
+                             #survey = "acs5",
+                             #output = "wide",
+                             #geometry = TRUE)
+
+  # this will return a table with 5 columns:
+  # GeoID, Name, med_earn, S2001_C01_002M (error), and geometry
+  # we only need med_earn (variable of interest) and geometry
+
+  census_var <- census_table[variable_n]
+
+}
+
+
 
 # this function will take in the inputs of census data (at whatever level) and
 # (preferably) city boundaries. Then it will crop the data to the boundaries.
 
 
+city_census_data <- function(census_var, bounds) {
+  ## census == census data, bounds == boundaries (shp., etc)
 
-city_census_data <- function(census, bounds) {  ## census == census data, bounds == boundaries (shp., etc)
   ### step 1: setting the CRS -------------------------------------
   # we need the CRS of the census data to match boundary data
   # we'll choose to use the census CRS, as this will be consistent
@@ -33,7 +60,7 @@ city_census_data <- function(census, bounds) {  ## census == census data, bounds
   # creating a new object -- boundary with amended CRS
   bounds_CRS <- st_transform(bounds, st_crs(census))
 
-  ### step 2: intersecting the bounds and the data
+  ### step 2: intersecting the bounds and the data ----------------
   city_int <- st_intersection(census, bounds_CRS)
 
   ## this ^ will be returned automatically! yay
